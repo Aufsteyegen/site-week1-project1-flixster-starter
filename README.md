cf7065158cce2da0946985508b3f5a440ef6376a
Gregory Caesar <main@Gregorys-MacBook-Pro.local>
2023-06-09 20:02:03 -0400

    finished. modal completed, event listeners added, fontawesome icons implemented (as well as "close" buttons), auto-complete for search done

--- README.md
+++ README.md
@@ -1,69 +1,78 @@
-📝 **NOTE** Use this template to initialize the contents of a README.md file for your application. As you work on your assignment over the course of the week, update the required or stretch features lists to indicate which features you have completed by changing `[ ]` to `[x]`. (🚫 Remove this paragraph before submitting your assignment.)
-
 ## Week 1 Assignment: Flixster
 
-Submitted by: **NAME**
+Submitted by: **GREGORY CAESAR**
 
-Estimated time spent: **#** hours spent in total
+Estimated time spent: **10** hours spent in total
 
-Deployed Application (optional): [Flixster Deployed Site](ADD_LINK_HERE)
+Deployed Application (optional): [Flixster Deployed Site]https://aufsteyegen.github.io/site-week1-project1-flixster-starter/
 
 ### Application Features
 
 #### Core Features
 
-- [ ] User can view a list of current movies from The Movie Database API as a grid view
+- [X] User can view a list of current movies from The Movie Database API as a grid view
   - The grid element should have an id of `movies-grid`
   - Each movie wrapper element should have a class of `movie-card`
-- [ ] For each movie displayed, user can see the following details:
+- [X] For each movie displayed, user can see the following details:
   - Title - the element should have a class of `movie-title`
   - Image - the `img` element should have a class of `movie-poster`
   - Votes - the element should have a class of `movie-votes`
-- [ ] User can load more current movies by clicking a button at the bottom of the list
+- [X] User can load more current movies by clicking a button at the bottom of the list
   - The button should have an id of `load-more-movies-btn`.
   - When clicked, the page should not refresh.
   - New movies should simply be added to the bottom
-- [ ] Allow users to search for movies and display them in a grid view
+- [X] Allow users to search for movies and display them in a grid view
   - There should be a search input element with an id of `search-input`
   - Users should be able to type into the input
   - When a user hits 'Enter', it should send a search request to the movies API
   - The results from the search should be displayed on the page
   - There should be a close icon with an id of `close-search-btn` that exits the search, clears results, and shows the current movies displayed previously
-- [ ] Website accounts for basic HTML/CSS accessibility features
-- [ ] Website should be responsive
+- [X] Website accounts for basic HTML/CSS accessibility features
+- [X] Website should be responsive
 
 #### Stretch Features
 
-- [ ] Deploy website using GitHub Pages.
-- [ ] Allow user to view more details about a movie within a popup.
-- [ ] Improve the user experience through CSS & animation.
+- [X] Deploy website using GitHub Pages.
+- [X] Allow user to view more details about a movie within a popup.
+- [X] Improve the user experience through CSS & animation.
 - [ ] Allow movie video trailers to be played using [embedded YouTube](https://support.google.com/youtube/answer/171780?hl=en)
 - [ ] Implement anything else that you can get done to improve the app functionality!
 
 ### Walkthrough Video
 
-`TODO://` Add the embedded URL code to your animated app walkthrough below, `ADD_EMBEDDED_CODE_HERE`. Make sure the video or gif actually renders and animates when viewing this README. (🚫 Remove this paragraph after adding walkthrough video)
-
-`ADD_EMBEDDED_CODE_HERE`
+<a href="https://www.loom.com/share/95e2ad36f1e34cb282b86ff08ea89c5a">
+    <img style="max-width:300px;" src="https://cdn.loom.com/sessions/thumbnails/95e2ad36f1e34cb282b86ff08ea89c5a-with-play.gif">
+</a>
 
 ### Reflection
 
 - Did the topics discussed in your labs prepare you to complete the assignment? Be specific, which features in your weekly assignment did you feel unprepared to complete?
 
-Add your response here
+Yes. I repeatedly referenced Lab 3 to guide myself through creating
+asynchronous functions, API calls (e.g., generating API URLs), integrating
+JavaScript with HTML (i.e., via the "querySelector" method), and adding event
+listeners to HTML elements. I felt prepared for implementing all features. 
 
 - If you had more time, what would you have done differently? Would you have added additional features? Changed the way your project responded to a particular event, etc.
   
-Add your response here
+I would have incorporated more accessibility features. I also would have
+implemented smooth CSS transitions for dynamically-loaded HTML elements. 
 
 - Reflect on your project demo, what went well? Were there things that maybe didn't go as planned? Did you notice something that your peer did that you would like to try next time?
 
-Add your response here
+I should have spent more time on the presentation; I would have been more persuasive
+had I included bullet points (rather than merely screenshots) to remind myself
+to mention/elaborate on various features. As Elena mentioned, I also should have framed flaws I noticed differently by saying "Had I had more time, I would have done X" rather than
+"This doesn't work at the moment," etc.
 
 ### Open-source libraries used
 
 - Add any links to open-source libraries used in your project.
+https://developer.themoviedb.org/reference/intro/getting-started
 
 ### Shout out
 
 Give a shout out to somebody from your cohort that especially helped you during your project. This can be a fellow peer, instructor, TA, mentor, etc.
+
+Thank you, Elena, for your consistent support this week. I appreciated your check-ins
+and your oppeness to answering questions.
--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-let curPage = 1
-let searchPage = 1
-let query = ""
-let queryMovieData = {}
-let curMovieRequestData = {}
+let curPage = 1, searchPage = 1
+let query = null, queryMovieData = null;
+let curMovieRequestData = null;
+let curMovieDetails = null;
+let pageExtendButton = null, pageFooter = null;
+let movieID = null;
 
 const movieGrid = document.querySelector('.movies-grid')
 const queryGrid = document.querySelector('.query-grid')
@@ -10,7 +11,8 @@ const queryGrid = document.querySelector('.query-grid')
 const searchButton = document.querySelector("#search-button")
 const searchInput = document.querySelector("#search-input")
 const searchForm = document.querySelector("#search-form")
-const pageExtendButton = document.querySelector('#extend-page')
+const queryCloseButton = document.querySelector("#close-search-btn")
+
 const options = {
     method: 'GET',
     headers: {
@@ -19,21 +21,74 @@ const options = {
     }
 };
 
-function displayResults(data) {
+function closeModal() {
+    document.querySelector('.movies-modal-inner').innerHTML = ``
+    document.querySelector('.movies-modal').style.display = "none"
+    document.querySelector('.movies-modal-inner').style.display = "none"
+}
+
+function displayMovieDetailsModal () {
+    document.querySelector('.movies-modal').style.display = "flex"
+    document.querySelector('.movies-modal-inner').style.display = "block"
+    const backdropURL = `https://image.tmdb.org/t/p/w500${curMovieDetails.backdrop_path}`;
+    document.querySelector('.movies-modal-inner').innerHTML += `
+    <img class="movie-backdrop" title="${curMovieDetails.original_title} movie backdrop."src="${backdropURL}"/>
+    <div class="modal-movie-title">
+    <div title="Movie title: ${curMovieDetails.original_title}">${curMovieDetails.original_title}</div>
+    <div title="Close modal" class="modal-close-button" onClick=closeModal()>
+        <button role="button">
+            <div title="Return to currently playing movies">
+                <i class="fa-solid fa-x fa-xl"></i>
+            </div>
+        </button>
+    </div>
+    </div>
+    <div title="Average rating: ${curMovieDetails.vote_average > 0 ? curMovieDetails.vote_average.toFixed(1) : "(No rating)"} out of 10"} class="modal-movie-votes">⭐️ ${curMovieDetails.vote_average > 0 ? curMovieDetails.vote_average.toFixed(1) : "—"}</div>
+    <div id="currently-playing-underline">━━━━━━━━━</div>
+    <div class="movie-tagline">${curMovieDetails.tagline}</div>
+    <div class="modal-movie-details">${curMovieDetails.overview}</div>
+    <br>
+    <div title="Runtime: ${curMovieDetails.runtime} minutes | Release date: ${curMovieDetails.release_date}" class="modal-movie-details">Runtime: ${curMovieDetails.runtime} minutes | Release date: ${curMovieDetails.release_date}</div>
+    `
+}
+
+async function triggerDetailsAPI (ID) {
+    movieID = ID
+    const url = `https://api.themoviedb.org/3/movie/${movieID}`
+    const response = await fetch(url, options);
+    const data = await response.json();
+    curMovieDetails = data;
+    displayMovieDetailsModal();
+}
+
+function displayResults (data) {
     const movies = data.results;
     offset = curPage * 20;
     movies.forEach(movie => {
         const imageURL = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
         movieGrid.innerHTML += `
-            <div class="movie-card">
-                <img title="${movie.title}" class="movie-poster" src="${imageURL}">
+            <div class="movie-card" id="${movie.id}" onClick="triggerDetailsAPI(${movie.id})">
+                <img class="movie-poster" title="${movie.title}" src="${imageURL}">
                 <div class="movie-description">
                     <div class="movie-title"><h4 >${movie.title}</h4></div>
-                    <div class="movie-votes" title="Average rating: ${movie.vote_average} out of 10" >⭐️ ${movie.vote_average}</h4></div>
+                    <div class="movie-votes" title="Average rating: ${movie.vote_average > 0 ? movie.vote_average.toFixed(1) : "(No rating)"} out of 10" >⭐️ ${movie.vote_average > 0 ? movie.vote_average.toFixed(1) : "—"}</h4></div>
                 </div>
-                </div>
+            </div>
         `
     })
+    if (curPage > 1) {
+        footer.remove();
+    }
+    movieGrid.innerHTML += `
+        <div id="footer">
+            <button title="Button to extend page" id="load-more-movies-btn">
+                <div><i class="fa-solid fa-angle-down fa-xl"></i></div>
+            </button>
+        </div>
+    `
+    pageExtendButton = document.querySelector('#load-more-movies-btn')
+    pageExtendButton.addEventListener("click", getMoreCurrentMovies)
+    pageFooter = document.querySelector('#footer')
     curPage += 1;
 }
 
@@ -41,59 +96,115 @@ async function getCurrentMovies() {
     const url = `https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=${curPage}`
     const response = await fetch(url, options);
     const data = await response.json();
-    console.log('Success! Request retrieved.');
     curMovieRequestData = data;
     displayResults(curMovieRequestData);
 }
 
 async function getMoreCurrentMovies(event) {
-    event.preventDefault()
     getCurrentMovies()
+    event.preventDefault()
 }
 
-pageExtendButton.addEventListener("click", getMoreCurrentMovies)
+function clearSearchInput (event) {
+    searchInput.value = "" 
+    event.preventDefault()
+}
+
+function closeQueryData() {
+    if (queryGrid.style.display == "flex") {
+    queryCloseButton.style.display = "none"
+    queryGrid.style.display = "none"
+    queryGrid.innerHTML = ``
+    movieGrid.style.display = "flex"
+    movieGrid.innerHTML = ``
+    searchPage = 1
+    getCurrentMovies()
+    document.querySelector('#currently-playing').textContent = "Now in theatres"
+    }
+}
+
+queryCloseButton.addEventListener("click", closeQueryData)
+queryCloseButton.addEventListener("click", clearSearchInput)
+
+async function getMoreSearchedMovies(event) {
+    searchPage += 1;
+    offset = searchPage * 20;
+    searchMovies()
+    event.preventDefault()
+}
 
 function displayQueryData() {
     const movies = queryMovieData.results;
-    offset = searchPage * 20;
+    queryCloseButton.style.display = "flex"
+    if (searchPage == 1) queryGrid.innerHTML = ``
     movies.forEach(movie => {
         const imageURL = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+        queryGrid.style.display = "flex"
         queryGrid.innerHTML += `
-            <div class="movie-card">
-                <img title="${movie.title}" class="movie-poster" src="${imageURL}">
+            <div class="movie-card" onClick="triggerDetailsAPI(${movie.id})">
+                <img class="movie-poster" title="${movie.title}" src="${imageURL}">
                 <div class="movie-description">
                     <div class="movie-title"><h4 >${movie.title}</h4></div>
-                    <div class="movie-votes" title="Average rating: ${movie.vote_average} out of 10" >⭐️ ${movie.vote_average}</h4></div>
+                    <div class="movie-votes" title="Average rating: ${movie.vote_average > 0 ? movie.vote_average.toFixed(1) : "No rating"} out of 10" >⭐️ ${movie.vote_average > 0 ? movie.vote_average.toFixed(1) : "—"}</h4></div>
                 </div>
             </div>
         `
     })
-    searchPage += 1;
+    if (searchPage > 1) {
+        document.querySelector('#footer2').remove();
+    }
+    queryGrid.innerHTML += `
+        <div id="footer2">
+            <button id="extend-search-page">
+                <div><i class="fa-solid fa-angle-down fa-xl"></i></div>
+            </button>
+        </div>
+    `
+    pageExtendButton = document.querySelector('#extend-search-page')
+    pageExtendButton.addEventListener("click", getMoreSearchedMovies)
 }
 
+
 async function searchMovies() {
+    movieGrid.style.display = "none"
     const url = `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=${searchPage}`
     const response = await fetch(url, options);
     const data = await response.json();
-    console.log('Success! Request retrieved.');
+    document.querySelector('#currently-playing').textContent = `Search results for: "${query}"`
     queryMovieData = data;
     displayQueryData();
 }
 
 async function handleFormSubmit(event) {
     curPage = 1
-    //moreButton.style.display = "block"
     query = searchInput.value
-    searchMovies()
-    console.log('The form was submitted.')
-    searchInput.value = ""
+    if (query.length > 0) {
+        searchMovies()
+        console.log('The form was submitted.')
+        searchInput.value = ""
+    }
     event.preventDefault()
 }
 
 searchForm.addEventListener("submit", handleFormSubmit)
 searchButton.addEventListener("click", handleFormSubmit)
 
+async function handleAutoComplete (event) {
+    curPage = 1
+    query = searchInput.value
+    if (query.length <= 2) {
+        closeQueryData()
+    }
+    else if (query.length >= 3) {
+        searchMovies()
+        console.log('The form was submitted.')
+    }
+    event.preventDefault()
+}
+
+searchForm.addEventListener("input", handleAutoComplete)
 
 window.onload = function () {
-    getCurrentMovies()    
+    getCurrentMovies()   
+    searchInput.value = ""
 } 
